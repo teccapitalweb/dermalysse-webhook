b2f3b357969dc8716b4ec48cfa2098eb6df3bec9
TEC Capital web <teccapitalweb@gmail.com>
2026-08-13 12:35:44 -0600

    Add files via upload

---
+++ dermalysse-bunny-catalog.js
@@ -0,0 +1,59 @@
+window.DERMALYSSE_BUNNY_CATALOG = [
+  {"titulo":"Uso de Tecnologías Avanzadas en Cosmiatría","collectionId":"dd16f07a-1654-4c09-b98d-ba4dfc428a5b","sesiones":[
+    {"titulo":"Uso de Tecnologías Avanzadas en Cosmiatría - Sesión 01","videoId":"93705efd-9188-48ab-b41e-67ebcaf692a0","duracionSegundos":6650},
+    {"titulo":"Uso de Tecnologías Avanzadas en Cosmiatría - Sesión 02","videoId":"8f0027f1-e45e-47f2-995a-1a503dc7cb66","duracionSegundos":7539}]},
+  {"titulo":"Técnicas Avanzadas en Rejuvenecimiento Facial - Botox y Rellenos Dérmicos","collectionId":"40cedcce-3167-4cc3-bbeb-7e247f37c87c","sesiones":[
+    {"titulo":"Técnicas Avanzadas en Rejuvenecimiento Facial - Botox y Rellenos Dérmicos - Sesión 01","videoId":"224422b4-9dc6-4f35-bba3-8ad82bdb8bff","duracionSegundos":4185},
+    {"titulo":"Técnicas Avanzadas en Rejuvenecimiento Facial - Botox y Rellenos Dérmicos - Sesión 02","videoId":"c2bf8765-9cab-4584-8b2e-97cfdd8ab276","duracionSegundos":7670},
+    {"titulo":"Técnicas Avanzadas en Rejuvenecimiento Facial - Botox y Rellenos Dérmicos - Sesión 03","videoId":"b3e737db-655d-4a56-bb08-b60660edcfbb","duracionSegundos":3474},
+    {"titulo":"Técnicas Avanzadas en Rejuvenecimiento Facial - Botox y Rellenos Dérmicos - Sesión 04","videoId":"46dc8080-e389-46da-9504-bf010f096969","duracionSegundos":3792}]},
+  {"titulo":"Tratamientos Faciales Anti-envejecimiento","collectionId":"8c9cdf6a-db86-4d02-960f-43df2d4fb368","sesiones":[
+    {"titulo":"Tratamientos Faciales Anti-envejecimiento - Sesión 01","videoId":"b1224b14-dafb-4c6f-9848-d07cd8f45450","duracionSegundos":7168},
+    {"titulo":"Tratamientos Faciales Anti-envejecimiento - Sesión 02","videoId":"31594ed2-5ef4-47bc-89ea-d80203064638","duracionSegundos":3575},
+    {"titulo":"Tratamientos Faciales Anti-envejecimiento - Sesión 03","videoId":"9df04def-eb0a-4fa8-b46d-13877d08e4bf","duracionSegundos":7800}]},
+  {"titulo":"Rejuvenecimiento Facial Eficaz - Domina el Electrolifting con Plasma Pen","collectionId":"4cfc65fb-d1bc-43ce-bf4b-1c1ae34900ac","sesiones":[
+    {"titulo":"Rejuvenecimiento Facial Eficaz - Domina el Electrolifting con Plasma Pen - Sesión 01","videoId":"bfa74672-7e1d-4d02-bb50-9dc437d841f7","duracionSegundos":4425},
+    {"titulo":"Rejuvenecimiento Facial Eficaz - Domina el Electrolifting con Plasma Pen - Sesión 02","videoId":"868448b8-3eca-49a0-9d8b-af3c14b159ee","duracionSegundos":4185},
+    {"titulo":"Rejuvenecimiento Facial Eficaz - Domina el Electrolifting con Plasma Pen - Sesión 03","videoId":"8aeb0cdb-73e3-456a-85eb-bebca26c3af6","duracionSegundos":6886},
+    {"titulo":"Rejuvenecimiento Facial Eficaz - Domina el Electrolifting con Plasma Pen - Sesión 04","videoId":"216a81c4-62a6-4a08-bfa5-97ce48837b98","duracionSegundos":2923}]},
+  {"titulo":"Regulación Cosmética","collectionId":"39120e40-06a8-467f-9e71-d23acbaf9efc","sesiones":[
+    {"titulo":"Regulación Cosmética - Sesiones 01 y 02","videoId":"209b227f-f986-42a9-a59b-7f90e0e1357b","duracionSegundos":7443},
+    {"titulo":"Regulación Cosmética - Sesiones 03 y 04","videoId":"5c12af55-00b2-43b0-977b-24bb2d6d2b35","duracionSegundos":7461},
+    {"titulo":"Regulación Cosmética - Sesión 05","videoId":"2a8de112-10d8-4945-91bf-4e750da20347","duracionSegundos":4317}]},
+  {"titulo":"Nutrición y Entrenamiento para Estética Corporal","collectionId":"a24d53a5-cea5-4bb2-b69d-c31f230601cb","sesiones":[
+    {"titulo":"Nutrición y Entrenamiento para Estética Corporal - Sesión 01","videoId":"2efde757-eccb-49d0-abdd-d3cd881bb5d7","duracionSegundos":3820},
+    {"titulo":"Nutrición y Entrenamiento para Estética Corporal - Sesión 02","videoId":"6cc17984-d7c1-41f6-a964-717d4c0f30d2","duracionSegundos":4836},
+    {"titulo":"Nutrición y Entrenamiento para Estética Corporal - Sesión 03","videoId":"81aee008-850c-42a6-89e9-2f5e5ceb6949","duracionSegundos":3574},
+    {"titulo":"Nutrición y Entrenamiento para Estética Corporal - Sesión 04","videoId":"2c4d14aa-dcff-4b7b-807a-f0a284233cd2","duracionSegundos":4104},
+    {"titulo":"Nutrición y Entrenamiento para Estética Corporal - Sesión 05","videoId":"e4c67fd4-901f-40fc-af90-7c39145981a3","duracionSegundos":3818}]},
+  {"titulo":"Nutrición para la Prevención del Sobrepeso y la Obesidad","collectionId":"3e791c52-3d5d-42e3-99fd-24381dacb5e9","sesiones":[
+    {"titulo":"Nutrición para la Prevención del Sobrepeso y la Obesidad - Sesión 01","videoId":"f76dbd85-6f48-4c0f-a884-3c26c6ebef86","duracionSegundos":6788},
+    {"titulo":"Nutrición para la Prevención del Sobrepeso y la Obesidad - Sesión 02","videoId":"df0a4a1d-6aaf-4740-a35b-791e20211eb3","duracionSegundos":6825},
+    {"titulo":"Nutrición para la Prevención del Sobrepeso y la Obesidad - Sesión 03","videoId":"2e8616b9-5e19-47e5-910b-d6b7f29fabc9","duracionSegundos":4153}]},
+  {"titulo":"Nutrición Clínica para Enfermedades Crónicas","collectionId":"e5b11b77-281a-4ec1-a746-955849e5e9d2","sesiones":[
+    {"titulo":"Nutrición Clínica para Enfermedades Crónicas - Sesión 01","videoId":"429d4726-4512-465a-accb-28e37ca2b235","duracionSegundos":3177},
+    {"titulo":"Nutrición Clínica para Enfermedades Crónicas - Sesión 02","videoId":"bef55fdb-90b7-435c-9e78-91d335ff9d8f","duracionSegundos":3592},
+    {"titulo":"Nutrición Clínica para Enfermedades Crónicas - Sesión 03","videoId":"ccd37109-67e2-4288-88e9-60f6876c4db3","duracionSegundos":3383},
+    {"titulo":"Nutrición Clínica para Enfermedades Crónicas - Sesión 04","videoId":"d636a465-8bb9-4f5e-86a3-fe8815dc0e1b","duracionSegundos":3661},
+    {"titulo":"Nutrición Clínica para Enfermedades Crónicas - Sesión 05","videoId":"95a4b27a-6652-46e0-b95c-2ea213cb64af","duracionSegundos":3459}]},
+  {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada","collectionId":"ae6b99ac-eebb-44a7-9d12-2c3e5ab1829b","sesiones":[
+    {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada - Sesión 01","videoId":"48558932-0c31-4c14-b057-b151ad8f3dd8","duracionSegundos":3821},
+    {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada - Sesión 02","videoId":"a0aa628d-724f-4470-947d-52dd88e41d30","duracionSegundos":3608},
+    {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada - Sesión 03","videoId":"43ffed71-00da-4e44-9947-c559c51a9006","duracionSegundos":3622},
+    {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada - Sesión 04","videoId":"41ca41ab-d702-47ae-9cc8-93ceaa8f4aaa","duracionSegundos":3317},
+    {"titulo":"Microdermoabrasión y Dermaplaning - Técnicas de Exfoliación Avanzada - Sesión 05","videoId":"936abc85-caf0-46cf-b772-2bbd0056af99","duracionSegundos":3307}]},
+  {"titulo":"Formulación de Cosmética Natural","collectionId":"5b72e3de-c814-4c9b-a8f3-279553caefc2","sesiones":[
+    {"titulo":"Formulación de Cosmética Natural - Sesiones 01 y 02","videoId":"b1d68d83-6482-4af3-b0e5-0ed31d60ddd5","duracionSegundos":8915},
+    {"titulo":"Formulación de Cosmética Natural - Sesiones 03, 04 y 05","videoId":"23cd1b4b-8542-49c9-83eb-6f6993403763","duracionSegundos":9060}]},
+  {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación","collectionId":"d896b5cb-3893-4360-8248-36532dd33675","sesiones":[
+    {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación - Sesión 01","videoId":"6cd97b3d-2fbf-4544-a0b7-31c72c73eb6a","duracionSegundos":4110},
+    {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación - Sesión 02","videoId":"405de1a1-a361-4a23-923c-fa3f8995984d","duracionSegundos":3632},
+    {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación - Sesión 03","videoId":"55f42b6f-4c71-4f80-9a78-cbadda4f55f9","duracionSegundos":3917},
+    {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación - Sesión 04","videoId":"a77baec6-6614-4f07-ba28-57b814c666fe","duracionSegundos":3568},
+    {"titulo":"Estética Corporal - Técnicas de Contorno y Remodelación - Sesión 05","videoId":"4ba28936-624a-432c-8708-c9ecf473988c","duracionSegundos":4283}]},
+  {"titulo":"Biohacking para la Salud - Nutrición y Metabolismo","collectionId":"5efef855-1d5b-49f3-bfae-e282d2c70e79","sesiones":[
+    {"titulo":"Biohacking para la Salud - Nutrición y Metabolismo - Sesión 01","videoId":"0e19122c-2efa-47be-8d3f-e092b54d5efa","duracionSegundos":4504},
+    {"titulo":"Biohacking para la Salud - Nutrición y Metabolismo - Sesión 02","videoId":"642700fd-7fd4-47d4-8c11-a72b5b55cca5","duracionSegundos":3812},
+    {"titulo":"Biohacking para la Salud - Nutrición y Metabolismo - Sesión 03","videoId":"e9cf0e2f-b309-4f88-ac63-78059258ceea","duracionSegundos":4285},
+    {"titulo":"Biohacking para la Salud - Nutrición y Metabolismo - Sesión 04","videoId":"ecab3ea5-b7a8-435c-ab52-7ca4a4a93d00","duracionSegundos":4366}]}
+];
